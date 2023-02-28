22e3beb6d56fcd94e8ece5fd4222f228aa1588b4
Nik <ice_fire50@mail.ru>
2023-02-28 15:23:56 +0300

    task 41

---
+++ task 41/Program.cs
@@ -0,0 +1,12 @@
+﻿int mass (int[] arr){
+    int N=0;
+    for (int i=0; i<arr.Length;i++){
+        if (arr[i]>0) N++;
+    }
+return N;
+}
+Console.Clear();
+Console.WriteLine("Введите массив");
+int[] arr = Console.ReadLine().Split(' ').Select(x=>int.Parse(x)).ToArray();
+Console.WriteLine($"[ {string.Join(" ", arr)} ]");
+Console.WriteLine(mass(arr));
